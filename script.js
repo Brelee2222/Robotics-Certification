ef283ac97866852fc476d0c4341c72ccab1c910d
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2022-09-12 17:03:36 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-document.getElementById("textThing").onclick = function() {
-    alert("hi");
+const button = document.querySelector("#textThing");
+button.onclick = function() {
+    alert("hilo wurld");
+    let text = button.appendChild(document.createElement("a"));
+    button.style.color = "#000000";
+    button.className = "why"
+    getData().then(res => res.json().then(json => button.innerText = `I have a server named ${json.name}`));
+}
+async function getData(){
+    return (await fetch("https://discord.com/api/guilds/807113739119755264/widget.json"));
 }
